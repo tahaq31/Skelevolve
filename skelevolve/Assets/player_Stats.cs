e424a7d547a7b53832600982fa0b789b8d68a7a7
tahaq31 <tehe3174@gmail.com>
2025-07-26 16:24:24 -0400

    tilemap and ui 1

--- skelevolve/Assets/player_Stats.cs
+++ skelevolve/Assets/player_Stats.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
 
 public class player_Stats : MonoBehaviour
 {
@@ -14,6 +16,8 @@ public class player_Stats : MonoBehaviour
     private int maxMP = 100;
     [SerializeField]
     private int currentMP;
+    public TextMeshProUGUI hP;
+    public TextMeshProUGUI mP;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +29,18 @@ public class player_Stats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        hP.text = "Health : " + currentHP + "/" + maxMP;
+        mP.text = "Mana : " + currentMP + "/" + maxMP;
+
+        //del later
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            currentHP--;
+        }
+    }
+    
+    public int getHP()
+    {
+        return this.currentHP;
     }
 }
